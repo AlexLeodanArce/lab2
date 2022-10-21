50b0722f63971d62dffee226709d18a5c6b96116
AlexLeodanArce <alexcanada201022@gmail.com>
2022-10-21 02:17:14 +0000

    Custom method "no_menores" has been added and some code has been rewritten

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,4 +1,12 @@
 class Attack < ApplicationRecord
     belongs_to :monster
     belongs_to :victim
+    validate :no_menores
+
+def no_menores
+    if monster.scare_level > 5 && victim.birthdate > 18.years.ago
+        errors.add(:monster_id, message:"No se puede agregar. Un monstruo de nivel de susto 5 no puede tener victimas menores de 18 años")
+    end
+end
+
 end
